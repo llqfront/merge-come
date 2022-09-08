6b384372a7bedf29465ad3381dc2a799b547e440
llqfront <llqfront@aliyun.com>
2022-09-08 10:10:52 +0800

    egg-mysql 配置多源数据

--- merge-com-egg/config/config.default.js
+++ merge-com-egg/config/config.default.js
@@ -14,7 +14,56 @@ module.exports = appInfo => {
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1662601071288_714';
-
+  
+  config.mysql = {
+    clients: {
+      // clientId, 获取client实例，需要通过 app.mysql.get('clientId') 获取
+      db1: {
+        // host
+        host: '127.0.0.1',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: '密码',
+        // 数据库名
+        database: '数据库名',
+      },
+      db2: {
+        // host
+        host: '127.0.0.1',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: '密码',
+        // 数据库名
+        database: '数据库名',
+      },
+      db3: {
+        // host
+        host: '127.0.0.1',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: '密码',
+        // 数据库名
+        database: '数据库名',
+      },
+      // ...
+    },
+    // 所有数据库配置的默认值
+    default: {
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
   // add your middleware config here
   config.middleware = [];
 
