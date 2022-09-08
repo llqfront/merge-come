4916d38781d26fc79c05f470dbe9ff9eb5080f41
llqfront <llqfront@aliyun.com>
2022-09-08 10:20:47 +0800

    egg-mysql 配置多源数据

--- merge-com-egg/config/config.default.js
+++ merge-com-egg/config/config.default.js
@@ -26,7 +26,7 @@ module.exports = appInfo => {
         // 用户名
         user: 'root',
         // 密码
-        password: '',
+        password: '11111',
         // 数据库名
         database: '数据库名',
       },
