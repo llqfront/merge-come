d0b92a2a3ee3ab8fbb837c8a59c1e133916c8ebf
llqfront <llqfront@aliyun.com>
2023-02-28 16:38:27 +0800

    up

---
+++ ultra-basic/server.tsx
@@ -0,0 +1,29 @@
+import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
+import { type Context, createServer } from "ultra/server.ts";
+import App from "./src/app.tsx";
+
+const server = await createServer({
+  importMapPath: import.meta.resolve("./importMap.json"),
+  browserEntrypoint: import.meta.resolve("./client.tsx"),
+});
+
+function ServerApp({ context }: { context: Context }) {
+  const requestUrl = new URL(context.req.url);
+
+  return <App />;
+}
+
+server.get("*", async (context) => {
+  /**
+   * Render the request
+   */
+  let result = await server.render(<ServerApp context={context} />);
+
+  return context.body(result, 200, {
+    "content-type": "text/html; charset=utf-8",
+  });
+});
+if (import.meta.main) {
+  serve(server.fetch);
+}
+export default server;
