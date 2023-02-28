cdb4fe21757c2317b6601888291cd112976b023b
llqfront <llqfront@aliyun.com>
2023-02-28 17:12:46 +0800

    router init

--- ultra-router-redux/src/app.tsx
+++ ultra-router-redux/src/app.tsx
@@ -1,5 +1,5 @@
 import useAsset from "ultra/hooks/use-asset.js";
-
+import Router from "./router.tsx";
 export default function App() {
   console.log("Hello world!");
   return (
@@ -12,27 +12,7 @@ export default function App() {
         <link rel="stylesheet" href={useAsset("/style.css")} />
       </head>
       <body>
-        <main>
-          <h1>
-            <span></span>__<span></span>
-          </h1>
-          <p>
-            Welcome to{" "}
-            <strong>Ultra</strong>. This is a barebones starter for your web
-            app.
-          </p>
-          <p>
-            Take{" "}
-            <a
-              href="https://ultrajs.dev/docs"
-              target="_blank"
-            >
-              this
-            </a>, you may need it where you are going. It will show you how to
-            customize your routing, data fetching, and styling with popular
-            libraries.
-          </p>
-        </main>
+        <Router/>
       </body>
     </html>
   );
--- ultra-router-redux/src/router.tsx
+++ ultra-router-redux/src/router.tsx
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 
 import useServerContext from "ultra/hooks/use-server-context.js";
 import { DefaultLayout } from "./layouts/DefaultLayout.tsx";
-const HomePage = lazy(() => import("~/pages/Home/index.tsx"));
-const AboutPage = lazy(() => import("~/pages/About/index.tsx"));
-const DemoPage = lazy(() => import("~/pages/Demo/index.tsx"));
+const HomePage = lazy(() => import("./pages/Home/index.tsx"));
+const AboutPage = lazy(() => import("./pages/About/index.tsx"));
+const DemoPage = lazy(() => import("./pages/Demo/index.tsx"));
 
 function RouteNotFound() {
   useServerContext((context) => {
@@ -15,18 +15,16 @@ function RouteNotFound() {
 }
 
 export default function Router() {
-    render(){
-        return(
-            <Suspense fallback={<div>Page is Loading...</div>}>
-              <Routes>
-                <Route path="/" element={<DefaultLayout />}>
-                  <Route index element={<HomePage />} />
-                  <Route path="about" element={<AboutPage />} />
-                  <Route path="demo" element={<DemoPage />} />
-                  <Route path="*" element={<RouteNotFound />} />
-                </Route>
-              </Routes>
-            </Suspense>
-        )
-    }
+    return(
+        <Suspense fallback={<div>Page is Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="demo" element={<DemoPage />} />
+              <Route path="*" element={<RouteNotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+    )
 }
